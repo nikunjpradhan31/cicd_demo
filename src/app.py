f06cae065d24cf23192dfd941c93ec1a464c07f1
Nikunj Pradhan <nikunj.pradhan31@gmail.com>
2025-12-08 00:11:29 -0600

    added more math functions and more tests

--- src/app.py
+++ src/app.py
@@ -1,2 +1,37 @@
+import math
+
 def add(a,b):
 	return a+b
+
+def sub(a,b):
+	return a-b
+
+def mul(a,b):
+	return a*b
+
+def div(a,b):
+	return a/b
+
+def sqrt(a):
+	return math.sqrt(a)
+
+def percent(a,b):
+	return a*b/100
+
+def log(a):
+	return math.log(a)
+
+def sin(a):
+	return math.sin(a)
+
+def cos(a):
+	return math.cos(a)
+
+def tan(a):
+	return math.tan(a)
+
+def square(a):
+	return a*a
+
+def cube(a):
+	return a*a*a
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,9 +4,123 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0,str(root / "src"))
 
-from app import add
+from app import *
 
 def test_add():
-	assert add(5,6) == 11
+    assert add(5, 6) == 11
+
 def test_add2():
-	assert add(5,6) != 10
+    assert add(-3, 7) == 4
+
+def test_add3():
+    assert add(0, 0) == 0
+
+
+def test_sub():
+    assert sub(10, 3) == 7
+
+def test_sub2():
+    assert sub(-5, -5) == 0
+
+def test_sub3():
+    assert sub(3, 10) == -7
+
+
+def test_mul():
+    assert mul(4, 5) == 20
+
+def test_mul2():
+    assert mul(-3, 6) == -18
+
+def test_mul3():
+    assert mul(0, 99) == 0
+
+
+def test_div():
+    assert div(10, 2) == 5
+
+def test_div2():
+    assert div(-9, 3) == -3
+
+def test_div3():
+    assert div(5, 2) == 2.5
+
+
+def test_sqrt():
+    assert sqrt(25) == 5
+
+def test_sqrt2():
+    assert sqrt(0) == 0
+
+def test_sqrt3():
+    assert math.isclose(sqrt(2), math.sqrt(2))
+
+
+def test_percent():
+    assert percent(50, 10) == 5
+
+def test_percent2():
+    assert percent(200, 25) == 50
+
+def test_percent3():
+    assert percent(0, 100) == 0
+
+
+def test_log():
+    assert log(1) == 0
+
+def test_log2():
+    assert math.isclose(log(math.e), 1)
+
+def test_log3():
+    assert log(10) == math.log(10)
+
+
+def test_sin():
+    assert math.isclose(sin(0), 0)
+
+def test_sin2():
+    assert math.isclose(sin(math.pi/2), 1)
+
+def test_sin3():
+    assert math.isclose(sin(math.pi), 0, abs_tol=1e-9)
+
+
+def test_cos():
+    assert math.isclose(cos(0), 1)
+
+def test_cos2():
+    assert math.isclose(cos(math.pi), -1)
+
+def test_cos3():
+    assert math.isclose(cos(math.pi/2), 0, abs_tol=1e-9)
+
+
+def test_tan():
+    assert math.isclose(tan(0), 0)
+
+def test_tan2():
+    assert math.isclose(tan(math.pi/4), 1)
+
+def test_tan3():
+    assert math.isclose(tan(-math.pi/4), -1)
+
+
+def test_square():
+    assert square(5) == 25
+
+def test_square2():
+    assert square(-4) == 16
+
+def test_square3():
+    assert square(0) == 0
+
+
+def test_cube():
+    assert cube(3) == 27
+
+def test_cube2():
+    assert cube(-2) == -8
+
+def test_cube3():
+    assert cube(0) == 0
